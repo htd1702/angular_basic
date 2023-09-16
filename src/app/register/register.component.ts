0b449097efe47b8125100ddece5f2ab5b1fc24a3
Dat.HoTien@vn.bosch.com <Dat.HoTien@vn.bosch.com>
2023-09-16 11:41:02 +0700

    2nd commit: register-app

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'app-register',
+  template:'<p> Register app <p>',
+  styles:['p{color: blue}'],
+
+})
+export class RegisterComponent {}
